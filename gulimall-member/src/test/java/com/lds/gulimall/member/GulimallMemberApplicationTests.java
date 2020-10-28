e2b1ecd4e695a0f9a3b4d020814d70e8d23773c6
lds <1019505281@qq.com>
2020-10-28 20:52:16 +0800

    初始化-创建好微服务模板

---
+++ gulimall-member/src/test/java/com/lds/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lds.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
